674dbb527ab34b06e6a4caef649e6b7fb63cd23d
iipekolict <iipekolict@gmail.com>
2021-09-01 23:15:59 +0300

    refactor: code refactoring

--- src/index.js
+++ src/index.js
@@ -4,20 +4,20 @@ module.exports = function solveSudoku(m) {
     const size = m.length
     const sec = Math.sqrt(size)
 
-    const get = () => {
+    const get = () => { // get first 0-cell
         for (let x = 0; x < size; x++) {
-            for (let y = 0; y < size; y++) if (m[x][y] === 0) return {x, y}
+            for (let y = 0; y < size; y++) if (m[x][y] === 0) return {x, y} // return coordinates
         }
-        return 42
+        return 42 // 0-cells ends (end of work)
     }
 
-    const val = (n, pos, m) => {
-        for (let i = 0; i < size; i++) if (i !== pos.x && m[i][pos.y] === n) return false
-        for (let i = 0; i < size; i++) if (i !== pos.y && m[pos.x][i] === n) return false
+    const val = (n, pos, m) => { // check validity
+        for (let i = 0; i < size; i++) if (i !== pos.x && m[i][pos.y] === n) return false // row
+        for (let i = 0; i < size; i++) if (i !== pos.y && m[pos.x][i] === n) return false // col
 
         const sectorR = Math.floor(pos.x / sec) * sec
         const sectorC = Math.floor(pos.y / sec) * sec
-        for (let i = sectorR; i < sectorR + sec; i++) {
+        for (let i = sectorR; i < sectorR + sec; i++) { // sector 3x3
             for (let j = sectorC; j < sectorC + sec; j++) {
                 if (m[i][j] === n && i !== pos.x && j !== pos.y) return false
             }
@@ -26,12 +26,12 @@ module.exports = function solveSudoku(m) {
         return true
     }
 
-    const go = () => {
-        const pos = get(m)
-        if (pos === 42) return true
+    const go = () => { // recurce fun
+        const pos = get(m) // get 0-cell from m
+        if (pos === 42) return true // if this = 42 -> end
 
-        for (let v of range) {
-            if (val(v, pos, m)) {
+        for (let v of range) { // iterate all variations
+            if (val(v, pos, m)) { // if valid
                 m[pos.x][pos.y] = v
                 if (go()) return true
                 m[pos.x][pos.y] = 0
